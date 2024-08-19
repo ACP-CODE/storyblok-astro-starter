bfafb734034d9993b37c51b5ab86b0c066601d51
Jaki Chen <JakiChen@live.cn>
2024-08-19 17:26:02 +0800

    Update

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,7 +2,7 @@ import type { MiddlewareHandler } from "astro";
 
 export const onRequest: MiddlewareHandler = ({ locals }, next) => {
   locals.seo = {
-    title: "Welcome to Astro + Storyblok CMS",
+    title: "Welcome to Astro -  start with Storyblok",
     description: "The boilerplate for astro project with Storyblok CMS.",
     // site: "@你的Twitter账号",
     // creator: "@ACPAMOY"
